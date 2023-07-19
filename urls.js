db4a658df00164caa5edd0da98d165f0703c145b
GHOST-Aram <owinoojala@gmail.com>
2023-07-19 08:33:04 +0300

    chore: Remove redundant import of app (app.use as use) in main urls

--- urls.js
+++ urls.js
@@ -1,16 +1,16 @@
-import { app, use } from './ghost/app/init.js';
+import { app } from './ghost/app/init.js';
 import { create404Error } from './ghost/utils/http-response.js';
 import { router as catalogRouter } from './catalog/urls.js'
 
-use(catalogRouter);
+app.use(catalogRouter);
 
 // catch 404 and forward to error handler
-use((req, res, next) =>{
+app.use((req, res, next) =>{
   	next(create404Error());
 });
 
 // error handler
-use((err, req, res, next) =>{
+app.use((err, req, res, next) =>{
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
