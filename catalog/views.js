2164810c4e57727096c1116e95776b15b6c3e9be
GHOST-Aram <owinoojala@gmail.com>
2023-07-19 08:22:33 +0300

    chore: refactor app init exports syntax

--- catalog/views.js
+++
@@ -1,6 +0,0 @@
-import { render } from "../ghost/utils/http-response.js";
-
-export const index = (req, res) =>{
-    render(res,'catalog/index', { title: 'Express' });
-}
-
--- ghost/app/init.js
+++ ghost/app/init.js
@@ -6,23 +6,20 @@ import morgan from 'morgan';
 import debuger from 'debug';
 import mongoose from 'mongoose';
 
-const app = express()
-const router = express.Router()
+export const app = express()
+export const router = express.Router()
 
-const server = http.createServer(app);
-const logger = morgan
-const debug = debuger('express-local-library-site:server');
-const createError = createHttpError
+export const server = http.createServer(app);
+export const logger = morgan
+export const debug = debuger('express-local-library-site:server');
+export const createError = createHttpError
 
-const Schema = mongoose.Schema
-const model = mongoose.model
+export const Schema = mongoose.Schema
+export const model = mongoose.model
 
-const use = (callback) =>{
+export const use = (callback) =>{
     app.use(callback)
 }
 
 
-export {
-    app, router, server, express, cookieParser, logger, debug,
-    createError, use, Schema,model, mongoose
-}
+export { express, cookieParser, mongoose }
