f0ead947f6af7778eeefd4e3935cdc9f6b5477eb
GHOST-Aram <owinoojala@gmail.com>
2023-07-21 10:26:44 +0300

    chore: Include Auth file

---
+++ db-auth.js
@@ -0,0 +1,4 @@
+const password = '5GWnUEeHmaSJxXx6'
+const dbname = ''
+export const dbUri = `mongodb+srv://ghost:${password}
+            @cluster0.xv0stty.mongodb.net/?retryWrites=true&w=majority`
